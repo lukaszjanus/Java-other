a0c9d3664f525dac634df70a45a3c3fffe012376
janus01@interia.pl <janus01@interia.pl>
2018-12-30 18:51:29 +0100

    Formatting

--- Zegar/src/Zegar.java
+++ Zegar/src/Zegar.java
@@ -17,13 +17,12 @@ import java.util.*;
  * 
  */
 
-
 public class Zegar {
 
 	public static void main(String[] args) {
 
 		JFrame okno = new JFrame("Zegar");
-		okno.setSize(400,150);
+		okno.setSize(400, 150);
 		Panel panel = new Panel();
 		okno.getContentPane().add(panel);
 		okno.setDefaultCloseOperation(3);
@@ -34,56 +33,68 @@ public class Zegar {
 }
 
 class Panel extends JPanel implements Runnable {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private int rok, mc, dzien, godz, min, sek;
 	private String smc, sdzien, sgodz, smin, ssek;
-	
-	Thread watek=null;
-	
-	public Panel() {		
+
+	Thread watek = null;
+
+	public Panel() {
 		watek = new Thread(this);
-		watek.start();		
+		watek.start();
 	}
-	
-	public void paintComponent(Graphics g)
-	{
-		g.clearRect(0, 0, 400,150);  //refreshing window before display next 'sekundy', 
-		//without this method digits are place up, without clean older digits 
-		Font czcionka = new Font("Monospaced", Font.BOLD,30);
+
+	public void paintComponent(Graphics g) {
+		g.clearRect(0, 0, 400, 150); // refreshing window before display next 'sekundy',
+		// without this method digits are place up, without clean older digits
+		Font czcionka = new Font("Monospaced", Font.BOLD, 30);
 		g.setFont(czcionka);
-		if(mc<9) smc="0"; else smc="";
-		if(dzien<10) sdzien="0"; else sdzien="";
-		g.drawString(rok+"."+smc+(mc+1)+"."+sdzien+dzien, 70, 40);
-		if(godz<10) sgodz="0"; else sgodz="";
-		if(min<10) smin="0"; else smin="";
-		if(sek<10) ssek="0"; else ssek="";
-		g.drawString(sgodz+godz+":"+smin+min+":"+ssek+sek, 70, 80);
+		if (mc < 9)
+			smc = "0";
+		else
+			smc = "";
+		if (dzien < 10)
+			sdzien = "0";
+		else
+			sdzien = "";
+		g.drawString(rok + "." + smc + (mc + 1) + "." + sdzien + dzien, 70, 40);
+		if (godz < 10)
+			sgodz = "0";
+		else
+			sgodz = "";
+		if (min < 10)
+			smin = "0";
+		else
+			smin = "";
+		if (sek < 10)
+			ssek = "0";
+		else
+			ssek = "";
+		g.drawString(sgodz + godz + ":" + smin + min + ":" + ssek + sek, 70, 80);
 	}
 
 	@Override
 	public void run() {
-		while(true)
-		{
-			GregorianCalendar czas=new GregorianCalendar();
-						
-			rok=czas.get(Calendar.YEAR);
-			mc=czas.get(Calendar.MONTH);
-			dzien=czas.get(Calendar.DAY_OF_MONTH);
-			godz=czas.get(Calendar.HOUR_OF_DAY);
-			min=czas.get(Calendar.MINUTE);
-			sek=czas.get(Calendar.SECOND);
-			
+		while (true) {
+			GregorianCalendar czas = new GregorianCalendar();
+
+			rok = czas.get(Calendar.YEAR);
+			mc = czas.get(Calendar.MONTH);
+			dzien = czas.get(Calendar.DAY_OF_MONTH);
+			godz = czas.get(Calendar.HOUR_OF_DAY);
+			min = czas.get(Calendar.MINUTE);
+			sek = czas.get(Calendar.SECOND);
+
 			try {
 				Thread.sleep(1000);
-			}catch(InterruptedException e)
-			{
+			} catch (InterruptedException e) {
 				System.out.println("Wystapil blad");
 			}
-			repaint(); //start method paintComponent -> change data in window
+			repaint(); // start method paintComponent -> change data in window
 		}
 	}
 }
